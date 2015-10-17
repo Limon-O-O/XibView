e950852da3fa42fa20cd93ac3953b687283a3efa
limon <limon@catch.la>
2015-10-17 14:47:24 +0800

    init

---
+++ XibView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XibView
+//
+//  Created by catch on 15/10/17.
+//  Copyright © 2015年 Limon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
